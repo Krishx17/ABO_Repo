d11255e0813bd1d0d33deff9a1e7674be84b196d
rudy45kc <krishneshchaurasia1@gmail.com>
2025-01-30 16:30:33 +0530

    search within radius feature push

--- force-app/main/default/lwc/searchDoctors/searchDoctors.js
+++ force-app/main/default/lwc/searchDoctors/searchDoctors.js
@@ -1,37 +1,122 @@
 import { LightningElement, track } from 'lwc';
-import getNearbyDoctors from '@salesforce/apex/DoctorSearchController.getNearbyDoctors';
+import getFilteredDoctors from '@salesforce/apex/DoctorSearchController.getFilteredDoctors';
+// import VerifyPhysicianHeader from '@salesforce/label/c.VerifyPhysicianHeader';
+// import VerifyPhysicianInstructions from '@salesforce/label/c.VerifyPhysicianInstructions';
 
-export default class SearchDoctors extends LightningElement {
-    @track radius = 10; // Default Radius (km)
+export default class DoctorLocationMap extends LightningElement {
     @track mapMarkers = [];
-    @track mapCenter = { location: { Latitude: 37.7749, Longitude: -122.4194 } }; // Default Center (San Francisco)
+    @track error;
+    @track isLoading = false;
+    @track noRecordsFound = false;
 
-    handleRadiusChange(event) {
-        this.radius = event.target.value;
+    // verifyPhysicianHeader = VerifyPhysicianHeader;
+    // verifyPhysicianInstructions = VerifyPhysicianInstructions;
+
+    // Variables to store user inputs
+    @track firstName = '';
+    @track lastName = '';
+    @track gender = '';
+    @track postalCode = '';
+    @track city = '';
+    @track state = '';
+    @track radius = 5;
+    @track userLat =  37.7749;
+    @track userLng = -122.4194;
+
+    // Dropdown Options
+    genderOptions = [
+        { label: 'Male', value: 'Male' },
+        { label: 'Female', value: 'Female' }
+    ];
+
+    // Capture input changes dynamically
+    handleInputChange(event) {
+        const field = event.target.dataset.field;
+        this[field] = event.target.value;
+        console.log(this[field]);
     }
 
-    filterMarkers() {
-        getNearbyDoctors({ radius: this.radius, userLat: 37.7749, userLong: -122.4194 }) 
-            .then((data) => {
-                this.mapMarkers = data.map(doctor => ({
-                    location: {
-                        City: doctor.City__c,
-                        Country: doctor.Country__c,
-                        PostalCode: doctor.PostalCode__c,
-                        State: doctor.State__c,
-                        Street: doctor.Street__c
-                    },
-                    title: doctor.Name,
-                    description: `City: ${doctor.City__c}, Phone: ${doctor.Phone__c}`
-                }));
+    validateInputs() {
+        return this.firstName || this.lastName || this.gender || this.postalCode || this.city || this.state;
+    }
 
-                // Set the center of the map dynamically
-                if (this.mapMarkers.length > 0) {
-                    this.mapCenter = this.mapMarkers[0]; 
+    handleSearch() {
+        if (!this.validateInputs()) {
+            this.error = 'Please enter at least one search criteria.';
+            this.mapMarkers = [];
+            this.noRecordsFound = true;
+            return;
+        }
+
+        this.isLoading = true;
+        this.error = null;
+        this.noRecordsFound = false;
+
+        const searchParams = {
+            firstName: this.firstName || null,
+            lastName: this.lastName || null,
+            gender: this.gender || null,
+            postalCode: this.postalCode || null,
+            city: this.city || null,
+            state: this.state || null,
+            radius: this.radius || null
+        };
+
+        // console.log(searchParams.firstName);
+        // console.log(searchParams.lastName);
+
+        getFilteredDoctors(searchParams)
+            .then((data) => {
+                if (data.length > 0) {
+                    this.mapMarkers = data.map((doctor) => ({
+                        location: {
+                            City: doctor.City__c,
+                            Country: doctor.Country__c,
+                            PostalCode: doctor.PostalCode__c,
+                            State: doctor.State__c,
+                            Street: doctor.Street__c,
+                            Gender:doctor.Gender__c
+                        },
+                        value: doctor.Id,
+                        title: doctor.Name,
+                        description: doctor.Description__c || 'No specialty provided',
+                        icon: 'standard:user'
+                    }));
+                    this.noRecordsFound = false;
+
+                    if (this.mapMarkers.length > 0) {
+                        this.mapCenter = this.mapMarkers[0]; 
+                    }
+                } else {
+                    this.mapMarkers = [];
+                    this.noRecordsFound = true;
                 }
+                this.error = undefined;
             })
             .catch((error) => {
-                console.error('Error fetching doctors:', error);
+                this.error = 'Error message: ' + error;
+                this.mapMarkers = [];
+                this.noRecordsFound = true;
+            })
+            .finally(() => {
+                this.isLoading = false;
             });
     }
+
+    get buttonLabel() {
+        return this.isLoading ? 'Searching...' : 'Search';
+    }
+    /*handleDoctorClick(event) {
+        const doctorId = event.target.dataset.id;
+
+        // Navigate to the doctor's record page
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: doctorId,
+                objectApiName: 'Doctor__c', // Replace with the API name of your object
+                actionName: 'view'
+            }
+        });
+    }*/
 }
